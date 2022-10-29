d301e9e2534c10f6fbc2474a599e7c1ace39ec58
wkdgkstjd <116706097+wkdgkstjd@users.noreply.github.com>
2022-10-29 13:13:58 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,7 @@ from streamlit_chat import message
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-import json
+import json  #프로그램 설치
 
 @st.cache(allow_output_mutation=True)
 def cached_model():
@@ -19,18 +19,25 @@ def get_dataset():
 model = cached_model()
 df = get_dataset()
 
-st.header('심리상담 챗봇')
-st.markdown("[득샘](http://www.dcoder.kr)")
+st.header('노잼도시 울산 되살리기 프로젝트')
+st.markdown("울산경의고등학교 코드포스에서 만든 울산을 홍보하기 워한 챗봇입니다!")
+st.markdown("울산의 여러 관광지들을 알려드립니다!")
+st.markdown("[울산경의고등학교](http://www.gyeongui.hs.kr)")
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
-
+    
+for i in range(len(st.session_state['past'])):
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
+        
 with st.form('form', clear_on_submit=True):
-    user_input = st.text_input('당신: ', '')
-    submitted = st.form_submit_button('전송')
+    user_input = st.text_input('질문하세요: ', '') #질문 칸
+    submitted = st.form_submit_button('전송') #전송 버튼 제작
 
 if submitted and user_input:
     embedding = model.encode(user_input)
@@ -39,9 +46,6 @@ if submitted and user_input:
     answer = df.loc[df['distance'].idxmax()]
 
     st.session_state.past.append(user_input)
-    st.session_state.generated.append(answer['챗봇'])
+    st.session_state.generated.append(answer['챗봇/대답'])
 
-for i in range(len(st.session_state['past'])):
-    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-    if len(st.session_state['generated']) > i:
-        message(st.session_state['generated'][i], key=str(i) + '_bot')
+
