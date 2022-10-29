595fb7b0116256d8885bb1a9ccd920be5d6f2d83
wkdgkstjd <116706097+wkdgkstjd@users.noreply.github.com>
2022-10-29 16:18:39 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -22,7 +22,7 @@ df = get_dataset()
 st.header('노잼도시 울산 되살리기 프로젝트')
 st.markdown("울산경의고등학교 코드포스에서 만든 울산을 홍보하기 워한 챗봇입니다!")
 st.markdown("울산의 여러 관광지들을 알려드립니다!")
-st.markdown("[울산경의고등학교](http://www.gyeongui.hs.kr)")
+st.markdown('[울산경의고등학교](http://www.gyeongui.hs.kr)')
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -30,14 +30,9 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
     
-for i in range(len(st.session_state['past'])):
-    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-    if len(st.session_state['generated']) > i:
-        message(st.session_state['generated'][i], key=str(i) + '_bot')
-        
 with st.form('form', clear_on_submit=True):
-    user_input = st.text_input('질문하세요: ', '') #질문 칸
-    submitted = st.form_submit_button('전송') #전송 버튼 제작
+    user_input = st.text_input('키워드를 입력하세요: ', '') #질문 칸
+    submitted = st.form_submit_button('클릭') #전송 버튼 제작
 
 if submitted and user_input:
     embedding = model.encode(user_input)
@@ -48,4 +43,7 @@ if submitted and user_input:
     st.session_state.past.append(user_input)
     st.session_state.generated.append(answer['챗봇/대답'])
 
-
+for i in range(len(st.session_state['past'])-1,-1,-1):
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
